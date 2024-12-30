f489f1dcbf3ba180f02687e00212724f8610086a
abimon <eddyabby59@gmail.com>
2024-12-30 12:59:29 +0300

    feature photo sized

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -293,43 +293,37 @@
     <div class="container py-5">
         <div class="row g-4 justify-content-center">
             <div class="col-md-6 col-lg-4">
-                <a href="#">
                     <div class="service-item bg-secondary rounded border border-secondary">
-                        <img src="/storage/front/img/featur-1.jpg" class="img-fluid rounded-top w-100" alt="">
+                        <img src="/storage/front/img/featur-1.jpg" class="img-fluid rounded-top w-100" style="height:45vh" alt="">
                         <div class="px-4 rounded-bottom">
                             <div class="service-content bg-primary text-center p-4 rounded">
                                 <h5 class="text-white">Fresh Fruits</h5>
-                                <h3 class="mb-0">20% OFF</h3>
+                                <!-- <h3 class="mb-0">20% OFF</h3> -->
                             </div>
                         </div>
                     </div>
-                </a>
             </div>
             <div class="col-md-6 col-lg-4">
-                <a href="#">
                     <div class="service-item bg-dark rounded border border-dark">
-                        <img src="/storage/front/img/featur-2.jpg" class="img-fluid rounded-top w-100" alt="">
+                        <img src="/storage/front/img/featur-3.jpg" class="img-fluid rounded-top w-100" style="height:45vh" alt="">
                         <div class="px-4 rounded-bottom">
                             <div class="service-content bg-light text-center p-4 rounded">
                                 <h5 class="text-primary">Exotic Vegetables</h5>
-                                <h3 class="mb-0">Tasty</h3>
+                                <!-- <h3 class="mb-0">Tasty</h3> -->
                             </div>
                         </div>
                     </div>
-                </a>
             </div>
             <div class="col-md-6 col-lg-4">
-                <a href="#">
                     <div class="service-item bg-primary rounded border border-primary">
-                        <img src="/storage/front/img/featur-3.jpg" class="img-fluid rounded-top w-100" alt="">
+                        <img src="/storage/front/img/featured-5.jpg" class="img-fluid rounded-top w-100" style="height:45vh" alt="">
                         <div class="px-4 rounded-bottom">
                             <div class="service-content bg-secondary text-center p-4 rounded">
                                 <h5 class="text-white">Quality Cereals</h5>
-                                <h3 class="mb-0">Discount 30$</h3>
+                                <!-- <h3 class="mb-0">Discount 30$</h3> -->
                             </div>
                         </div>
                     </div>
-                </a>
             </div>
         </div>
     </div>
